d97cedda8629c764e48fe88d69054d98fe2be2b2
Bangwahyu14 <whagung553@gmail.com>
2024-06-14 14:08:34 +0700

    membuat root newnode dan if parent

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -26,7 +26,20 @@ public:
 	void insert(string element) {// Insert a Node in a Binary Search Tree
 		Node* newNode = new Node(element, NULL, NULL); // Allocate memmory for the new node
 		newNode->info = element;  // Assign to the data field of the new node
+		newNode->leftchild = NULL;  //Make the left child of the new node point to null
+		newNode->rightchild = NULL; // Make the right child of the new node point to null
 
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+
+		search(element, parent, currentNode);   // Locate the node which will be the parent of the new node
+
+
+		if (parent == NULL) //if the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode; //mark the new node as root
+			return;
+		}
 
 	}
 }
